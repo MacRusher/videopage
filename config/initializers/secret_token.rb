3a69dc7fa9584372e74b79fc9624f655d4f0b2ee
Maciek <macrusher@gmail.com>
2012-09-10 06:52:20 -0700

    Poczatek piekniej przygody

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Videopage::Application.config.secret_token = '201f1eda1aa229d2cb9f80845e76dc3fd8af4c1072534415c51a653e39794e6e67b30dd0f75054df1b15b7eb107bf07897c796d6d2e5445ade3f8ef447b6f50e'
