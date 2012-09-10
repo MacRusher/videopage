629c42a36e8bccf90578e8143d6f59d67a43eaab
Maciek <macrusher@gmail.com>
2012-09-10 07:30:54 -0700

    Zainstalowany bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Videopage::Application.routes.draw do
+  
+  root :to => 'pages#index'
+  
+  resources :pages
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
